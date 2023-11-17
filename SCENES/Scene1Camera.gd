cbe1ee1663755af6966f40b559e4a1287abc1cf2
ethanurl <eearle@ucsc.edu>
2023-11-17 14:43:04 -0800

    WHY is the camera different on mac and windows

--- SCENES/Scene1Camera.gd
+++ SCENES/Scene1Camera.gd
@@ -3,10 +3,12 @@ extends Camera2D
 @onready var camera = $"."
 
 func _process(delta):
+	print(camera.position)
+	print(get_viewport().get_mouse_position())
 	var j = get_viewport().get_mouse_position()
 	if camera.position.x < 562:
 		if j.x > 852:
-			camera.position.x += (j.x - 852) * 0.002
+			camera.position.x += (j.x - 852) * 0.02
 	if camera.position.x > -562:
 		if j.x < 400:
-			camera.position.x -= (400 - j.x) * 0.002
+			camera.position.x -= (400 - j.x) * 0.02
