d750bf82494dadbdba97e936f4d3c6aa007ca68d
ethanurl <eearle@ucsc.edu>
2023-11-17 16:28:13 -0800

    Changed Completely from Godot to Renpy

--- README.md
+++
@@ -1,3 +0,0 @@
-# camp-cave-game
-170 Jam #3 - 12 different endings and escape room
-README!!!!! WOOOOOOOOO
--- SCENES/scene_2.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _process(_delta):
-	if Input.is_action_pressed("SwapScene"):
-		$Scene2Camera/Scene2Fader.visible = true
-		$Scene2Camera/Scene2Fader/SCENE2FADER.play("Fade")
-
-func _on_scene_2fader_animation_finished(anim_name):
-	get_tree().change_scene_to_file("res://SCENES/main_scene.tscn")
